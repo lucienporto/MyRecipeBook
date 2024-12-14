78f629555b288b020157190efc8a0e1c10f2f351
Lucien Porto <136341310+lucienporto@users.noreply.github.com>
2024-12-14 19:39:04 -0300

    feat: adicionando validator para receitas

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipes/RecipeValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+
+namespace MyRecipeBook.Application.UseCases.Recipes
+{
+    public class RecipeValidator : AbstractValidator<RequestRecipeJson>
+    {
+        public RecipeValidator()
+        {
+            RuleFor(recipe => recipe.Title).NotEmpty().WithMessage(ResourceMessageException.RECIPE_TITLE_EMPTY);
+            RuleFor(recipe => recipe.CookingTime).IsInEnum().WithMessage(ResourceMessageException.COOKING_TIME_NOT_SUPPORTED);
+            RuleFor(recipe => recipe.Difficulty).IsInEnum().WithMessage(ResourceMessageException.DIFFICULTY_LEVEL_NOT_SUPPORTED);
+        }
+    }
+}

751d2adaa748e882c758475bad2774273460f8f3
Lucien Porto <136341310+lucienporto@users.noreply.github.com>
2024-12-17 18:10:53 -0300

    feat: adicionando respositorio, dbcontext e injeção de dependencia de infraestrutura

--- src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.Recipe;
 using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Domain.Security.Criptography;
 using MyRecipeBook.Domain.Security.Tokens;
@@ -48,6 +49,7 @@ namespace MyRecipeBook.Infrastructure
             services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
             services.AddScoped<IUserReadOnlyRepository, UserRepository>();
             services.AddScoped<IUserUpdateOnlyRepository, UserRepository>();
+            services.AddScoped<IRecipeWriteOnlyRepository, RecipeRepository>();
         }
 
         private static void AddFluentMigrator(IServiceCollection services, IConfiguration configuration)

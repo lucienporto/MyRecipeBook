328a10c0865f2115a5cc82d2e868e7241e5d6008
Lucien Porto <136341310+lucienporto@users.noreply.github.com>
2025-01-07 11:33:54 -0300

    feat: add use case para filtrar receitas

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipes/Filter/IFilterRecipeUseCase.cs
@@ -0,0 +1,10 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.Recipes.Filter
+{
+    public interface IFilterRecipeUseCase
+    {
+        public Task<ResponseRecipesJson> Execute(RequestFilterRecipeJson request);
+    }
+}

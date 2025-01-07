b13574a12d3a03aad4d9afe7660bcb9d4ab46c2e
Lucien Porto <136341310+lucienporto@users.noreply.github.com>
2025-01-07 11:31:20 -0300

    feat: add request para filtrar receitas

---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestFilterRecipeJson.cs
@@ -0,0 +1,12 @@
+﻿using MyRecipeBook.Communication.Enums;
+
+namespace MyRecipeBook.Communication.Requests
+{
+    public class RequestFilterRecipeJson
+    {
+        public string? RecipeTitle_Ingredient { get; set; }
+        public IList<CookingTime> CookingTime { get; set; } = [];
+        public IList<Difficulty> Difficulty { get; set; } = [];
+        public IList<DishTypes> DishTypes { get; set; } = [];
+    }
+}

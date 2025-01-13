53cabc00c25c7c1d42fa5b23aa10c15245a1626a
Lucien Porto <136341310+lucienporto@users.noreply.github.com>
2025-01-13 14:26:56 -0300

    feat: add dtos para filtrar receitas

---
+++ src/Backend/MyRecipeBook.Domain/Dtos/FilterRecipesDto.cs
@@ -0,0 +1,13 @@
+﻿using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Domain.Dtos
+{
+    public record FilterRecipesDto
+    {
+        public string? RecipeTitle_Ingredient { get; init; }
+        public IList<CookingTime> CookingTime { get; init; } = [];
+        public IList<Difficulty> Difficulty { get; init; } = [];
+        public IList<DishTypes> DishTypes { get; init; } = [];
+
+    }
+}
